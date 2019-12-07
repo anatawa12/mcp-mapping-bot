a96d1cb74f6ec678842fa57b782df293cdaeec97
anatawa12 <anatawa12@icloud.com>
2019-12-07 22:44:28 +0900

    add ChannelBotGetter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {getVersions} from "./mcpMappingManager";
-import * as discord from "discord.js";
+import {getVersions, MCPVersions} from "./mcpMappingManager";
 import Conf from "conf";
+import {BotMain} from "./BotMain";
+import {Future} from "./Future";
 
 //main
-(async ()=>{
+(async () => {
     const configFile = new Conf<ConfigFile>({
         defaults: {
             "version-channels": {},
@@ -15,9 +16,33 @@ import Conf from "conf";
         .filter((mc) => versions.getMCVersions().indexOf(mc) == -1)
         .forEach((mc) => {
             console.warn(`mc(${mc}) is not found from mcp versions`)
-        })
-    discord.Client
+        });
+
+    const getter = new ChannelBotGetter(versions);
+
 })();
+
+class ChannelBotGetter {
+    versions: MCPVersions;
+    private cache: { [index: string]: Future<BotMain> | undefined } = {}
+
+    constructor(versions: MCPVersions) {
+        this.versions = versions
+    }
+
+    async get(version: string): Promise<BotMain> {
+        if (this.cache[version]) {
+            return await this.cache[version]!.promise()
+        } else {
+            const bot = new BotMain(version);
+            const future = this.cache[version] = new Future();
+            bot.init(this.versions)
+                .then(() => future.resolve(bot), e => future.reject(e));
+
+            return await future.promise();
+        }
+    }
+}
 
 interface ConfigFile {
     "version-channels": { [mc: string]: string[] }
